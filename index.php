b202153f0a580d178e70f21be9abc4d0f068bafd
truongnhatphi <55117983+truongnhatphi@users.noreply.github.com>
2020-04-23 15:49:58 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-
+<script>
+    function getdata()
+	{
+		var a;
+		a = <?php echo $1?>;
+		document.getElementById("ketqua").value = a;
+	}
+</script>
 
 <!DOCTYPE html>
 <html lang="vi" xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
     <title>Trương Nhật Phi</title>
     <meta charset="UTF-8">
@@ -9,48 +17,39 @@
 </head>
 <body>
 <h1>Xét Tam giác :</h1>
-
-<form method="POST">
-<div> Nhập canh A </div>
-<input name="a" type="text" id ="a">
-<br>
-<div> Nhập canh B </div>
-<input name="b" type="text" id ="b">
-<br>
-<div> Nhập canh C </div>
-<input name="c" type="text" id ="c">
 <?php
-	if(isset($_POST["a"]) && isset($_POST["b"]) && isset($_POST["c"]))
+	if(isset($_GET["a"]) && isset($_GET["b"]) && isset($_GET["c"]))
 		{
-			$a = $_POST["a"];
-			$b = $_POST["b"];
-			$c = $_POST["c"];
+			$a = $_GET["a"];
+			$b = $_GET["b"];
+			$c = $_GET["c"];
 			if( $a +$b > $c && $a+$c>$b && $b+$c > $a)
 			{
 				if($a==$b && $b==$c)
-				$l = "Tam Giác Đều ";
+				$1 = "Tam Giác Đều ";
 				elseif($a==$b || $a==$c || $b==$c )
-				$l = "Tam Giác Cân ";
+				$1 = "Tam Giác Cân ";
 				elseif(pow($a,2)==pow($b,2)+pow($c,2)||pow($b,2)==pow($c,2)+pow($c,2)|| pow($c,2)==pow($b,2)+pow($a,2))
-				$l = "Tam Giác Vuông ";
+				$1 = "Tam Giác Vuông ";
 				elseif(($a==$b || $a==$c || $b==$c ) && (pow($a,2)==pow($b,2)+pow($c,2)||pow($b,2)==pow($c,2)+pow($c,2)|| pow($c,2)==pow($b,2)+pow($a,2)))
-				$l = "Tam Giác Vuông Cân ";
+				$1 = "Tam Giác Vuông Cân ";
 				else
-				$l = "Tam Giác Thường ";
+				$1 = "Tam Giác Thường ";
 			}
 			else
-			$l = " Đây không phải tam giác ";
+			$1 = " Đây không phải tam giác ";
 		}
 		
 ?>
-
-<input type="submit" name="click" value="Check"/>
-<?php
-	if(isset($_POST['click']))
-	{
-		echo $l;
-	}
-?>
+<form nethod="GET">
+<div> Nhập canh A </div>
+<input name="a" type="text" id ="a" value="<?php echo $_GET["a"];?>" />
+<br>
+<div> Nhập canh B </div>
+<input name="b" type="text" id ="b" value="<?php echo $_GET["b"];?>" />
+<br>
+<div> Nhập canh C </div>
+<input name="c" type="text" id ="c" value="<?php echo $_GET["c"];?>" />
+<input name="ketqua">
+<input type="button" name="check" onclick="getdata()" />
 </form>
-</body>
-</html>
